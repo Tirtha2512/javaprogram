4029590025dd614bd0f3ae2d36815a33fac953db
Tirtha2512 <tirthagohel159@gmail.com>
2026-07-09 09:57:12 +0530

    Add files via upload

---
+++ scannertest.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class scannertest
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter rollno=");
+		int roll=sc.nextInt();
+		System.out.println("Enter Name=");
+		String nm=sc.next();
+		System.out.println("Enter fees=");
+		double f=sc.nextDouble();
+		System.out.println("Enter stream=");
+		String s=sc.next();
+		System.out.println("Rollno="+roll+"\nName="+nm+
+		"\nFees="+f+"Stream="+s);
+	}
+}
+
+		
+		
+		
+		
+		
+		
