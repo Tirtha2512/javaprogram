0cd35fe794b1531af22086384a12328eabe7a6dc
Tirtha2512 <tirthagohel159@gmail.com>
2026-07-10 08:12:17 +0530

    Add files via upload

---
+++ test_switch.java
@@ -0,0 +1,18 @@
+class test_switch
+{
+	public static void main (String args[])
+	{
+		int x=Integer.parseInt(args[0]);
+		switch(x)
+		{
+		case 1:System.out.println("Monday");break;
+		case 2:System.out.println("Tuesday");break;
+		case 3:System.out.println("Wensday");break;
+		case 4:System.out.println("thursday");break;
+		case 5:System.out.println("Friday");break;
+		case 6:System.out.println("Saturday");break;
+		case 7:System.out.println("Sunday");break;
+		default :System.out.println("Invalid numbers of day");
+		}
+	}
+}
