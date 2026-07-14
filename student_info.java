967274a1a8303ad4444002ecb925dba3aa27a95e
Tirtha2512 <tirthagohel159@gmail.com>
2026-07-14 09:16:14 +0530

    Add files via upload

---
+++ student_info.java
@@ -0,0 +1,15 @@
+/*class and object*/
+class student_info
+{
+	int id=1;
+	String name="ram";
+	public static void main(String args[])
+	{
+		student_info s=new student_info();
+		//s.id=1;
+		//s.name=jay;
+		System.out.println(s.id);
+		System.out.println(s.name);
+
+	}
+}
