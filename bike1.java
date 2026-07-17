a08d0f391dfe8538c8cea5981d934db650d48438
Tirtha2512 <tirthagohel159@gmail.com>
2026-07-17 08:17:00 +0530

    Add files via upload

---
+++ bike1.java
@@ -0,0 +1,14 @@
+class bike1
+{
+	//creating a default constructor
+	bike1()
+	{
+		System.out.println("bike is created");
+	}
+	//main method
+	public static void main(String args[])
+	{
+		//calling a default constructor
+		bike1 b=new bike1();
+	}
+}
